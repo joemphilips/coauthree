2ddb622d416d16da3ff73c37a1a4d044eb09caf4
joemphilips <joemphilips@gmail.com>
2016-10-27 23:51:41 +0900

    start writing coauthree

---
+++ coauthree/js/coauthree.js
@@ -0,0 +1,19 @@
+/*jslint        browser : true, continue : true,
+ devel : true, indent : 2, maserr
+
+
+
+
+*/
+
+var coauthree = (function () {
+    var main = function ( $container ) {
+        container = document.getElementById( $container );
+
+        var globe = new THREE.Globe( container );
+
+        var xhr = new XMLHttpRequest();
+        xhr.open( 'GET', )
+    };
+    return { initModule: initModule };
+}());
